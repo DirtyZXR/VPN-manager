79044b58e139bed29575a00a014e51d08d32f244
denis <d.zelinskyi@yandex.ru>
2026-04-21 15:56:59 +0300

    Доабвлено переиспользование токена

--- check_syntax.py
+++ check_syntax.py
@@ -1,24 +1,10 @@
-import os
-import py_compile
+import compileall
 import sys
 
 
 def check_syntax(directory):
-    errors = False
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".py"):
-                filepath = os.path.join(root, file)
-                try:
-                    py_compile.compile(filepath, doraise=True)
-                except py_compile.PyCompileError as e:
-                    print(f"Syntax error in {filepath}:\n{e}\n")
-                    errors = True
-                except Exception as e:
-                    print(f"Error compiling {filepath}: {e}\n")
-                    errors = True
-
-    if not errors:
+    success = compileall.compile_dir(directory, force=True)
+    if success:
         print(f"All .py files in {directory} have valid syntax.")
     else:
         sys.exit(1)
